8a420a2ec9123d29636b823a31dd0ef893ffad36
Loosee64 <lucyarthur1709@gmail.com>
2025-09-24 09:36:37 +0100

    Basic Dialogue toggle done

--- Project/scripts/toggle.gd
+++ Project/scripts/toggle.gd
@@ -2,7 +2,22 @@ extends Button
 
 
 
-func _on_pressed() -> void:
-	get_node("Dialogue").get_child(1).hide()
-	get_node("Dialogue").get_child(2).hide()
+
+
 	
+	
+
+
+func _on_pressed() -> void:
+	if get_node("../Player").get_child(0).visible == true:
+		get_node("../Player").get_child(0).hide()
+		get_node("../Player").get_child(1).hide()
+	
+		get_node("../NPC").get_child(0).hide()
+		get_node("../NPC").get_child(1).hide()
+	else:
+		get_node("../Player").get_child(0).show()
+		get_node("../Player").get_child(1).show()
+	
+		get_node("../NPC").get_child(0).show()
+		get_node("../NPC").get_child(1).show()
